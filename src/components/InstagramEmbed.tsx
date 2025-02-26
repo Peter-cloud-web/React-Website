6d7b1338ac8d53f46bfa94f54e090b2d586b9769
Peter-cloud-web <petergichia165@gmail.com>
2025-02-26 23:31:00 +0300

    Remove unused components

--- src/components/InstagramEmbed.tsx
+++ src/components/InstagramEmbed.tsx
@@ -1,30 +1,27 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 
-interface InstagramEmbedProps {
-  url: string;
-}
+// interface InstagramEmbedProps {
+//   url: string;
+// }
 
-const InstagramEmbed: React.FC<InstagramEmbedProps> = ({ url }) => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.async = true;
-    script.src = "//www.instagram.com/embed.js";
-    document.body.appendChild(script);
+// const InstagramEmbed: React.FC<InstagramEmbedProps> = ({ url }) => {
+//   useEffect(() => {
+//     const script = document.createElement("script");
+//     script.async = true;
+//     script.src = "//www.instagram.com/embed.js";
+//     document.body.appendChild(script);
 
-    if (window.instgrm) {
-      window.instgrm.Embeds.process();
-    }
-  }, [url]);
+//   }, [url]);
 
-  return (
-    <div className="instagram-embed">
-      <blockquote
-        className="instagram-media"
-        data-instgrm-permalink={url}
-        data-instgrm-version="13"
-      ></blockquote>
-    </div>
-  );
-};
+//   return (
+//     <div className="instagram-embed">
+//       <blockquote
+//         className="instagram-media"
+//         data-instgrm-permalink={url}
+//         data-instgrm-version="13"
+//       ></blockquote>
+//     </div>
+//   );
+// };
 
-export default InstagramEmbed;
+// export default InstagramEmbed;
--- src/main.tsx
+++ src/main.tsx
@@ -10,5 +10,5 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
+
  
